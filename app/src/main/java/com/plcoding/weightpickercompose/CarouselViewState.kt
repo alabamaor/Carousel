93bca011b4afece857ea559f29c27869fb27a18b
Albina Oriyaev <>
2021-12-08 11:36:36 +0200

    no message

--- app/src/main/java/com/plcoding/weightpickercompose/CarouselViewState.kt
+++
@@ -1,7 +0,0 @@
-package com.plcoding.weightpickercompose
-
-sealed class CarouselViewState {
-    //    data class MiddlePoint(val isDrag: Boolean) : CarouselViewState()
-    object RangedMiddlePoint : CarouselViewState()
-    object OtherPoint : CarouselViewState()
-}
